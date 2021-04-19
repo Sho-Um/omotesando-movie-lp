afa12a925cc4ad1ec308100e6f2935c6928e6281
Sho-Um <73683252+Sho-Um@users.noreply.github.com>
2021-04-19 09:01:38 +0900

    ローディング時のアニメーション設定

--- docs/js/script.js
+++ docs/js/script.js
@@ -94,3 +94,10 @@ $(window).on('load', function(){
 		$(Box).slideDown(500);					//アコーディオンを開く
 	});
 });
+
+
+//logoの表示
+$(window).on('load',function(){
+	$("#splash").delay(2750).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+	$("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+});
